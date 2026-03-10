ce45f02e926ca97ebb1369c74a7b7ee767e4429f
Safwane Bada <safwaneb1@gmail.com>
2026-03-10 11:11:26 +0100

    cron

--- mails/cron.js
+++ mails/cron.js
@@ -5,6 +5,7 @@ const { envoyerRappelUtilisateursInactifs } = require('../mails/mailService'); /
  * SB - Tâche planifiée (cron job) pour vérifier les utilisateurs inactifs
  * S'exécute toutes les minutes pour tester (à ajuster en production)
  * En production: '0 8 1 * *' pour s'exécuter le 1er de chaque mois à 8h00
+ * /1 * * * * pour chaque minute (pour tests)
  */
 // Planifie l'exécution le 1er de chaque mois à 8h00
 cron.schedule('0 8 1 * *', async () => {
@@ -12,7 +13,7 @@ cron.schedule('0 8 1 * *', async () => {
   try {
     // SB - Appel de la fonction pour envoyer les rappels
     await envoyerRappelUtilisateursInactifs();
-    console.log("📬 Rappels envoyés avec succès !");
+    console.log("Rappels envoyés avec succès !");
   } catch (error) {
     console.error("❌ Erreur lors de l'envoi des rappels :", error.message);
   }
